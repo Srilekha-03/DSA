b296f8c31fc4b1e1ebd9832182ed368f7043fed4
Srilekha-03 <147143658+Srilekha-03@users.noreply.github.com>
2024-12-27 20:47:56 +0530

    Time: 31 ms (55.62%), Space: 18.8 MB (16.45%) - LeetHub

---
+++ 0142-linked-list-cycle-ii/0142-linked-list-cycle-ii.py
@@ -0,0 +1,27 @@
+# Definition for singly-linked list.
+# class ListNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution(object):
+    def detectCycle(self, head):
+        slow=head
+        fast=head
+        while fast!=None and fast.next!=None:
+            slow=slow.next
+            fast=fast.next.next
+            if slow==fast:
+                slow=head
+                count=1
+                while slow!=fast:
+                    slow=slow.next
+                    count+=1
+                    fast=fast.next
+                return slow
+        return None 
+        """
+        :type head: ListNode
+        :rtype: ListNode
+        """
+        
