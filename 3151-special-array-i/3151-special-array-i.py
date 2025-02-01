1f8af8a6956920883c2816181b36fecd958adc25
Srilekha Kasha <147143658+Srilekha-03@users.noreply.github.com>
2025-02-01 22:16:10 +0530

    Time: 1 ms (35.36%), Space: 17.7 MB (33.33%) - LeetHub

---
+++ 3151-special-array-i/3151-special-array-i.py
@@ -0,0 +1,14 @@
+class Solution:
+    def isArraySpecial(self, nums: List[int]) -> bool:
+        i=0
+        j=1
+        while j<len(nums):
+            if nums[i]%2==0 and nums[j]%2==0:
+                return False
+            if nums[i]%2==1 and nums[j]%2==1:
+                return False
+            i+=1
+            j+=1
+        return True
+
+        
