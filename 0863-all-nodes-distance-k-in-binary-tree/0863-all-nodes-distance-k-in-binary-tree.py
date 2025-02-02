3c959964b03d54fd1752e925cc0d64475c6be5f4
Srilekha Kasha <147143658+Srilekha-03@users.noreply.github.com>
2025-02-02 14:10:04 +0530

    Time: 45 ms (32.21%), Space: 18.2 MB (34.6%) - LeetHub

---
+++ 0863-all-nodes-distance-k-in-binary-tree/0863-all-nodes-distance-k-in-binary-tree.py
@@ -0,0 +1,50 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution:
+    def distanceK(self, root: TreeNode, target: TreeNode, k: int) -> List[int]:
+        if root is None:
+            return []
+        parent=self.parentMap(root)
+        q=deque()
+        visited=set()
+        q.append(target)
+        visited.add(target)
+        cur_level=0
+        while q:
+            if cur_level==k:
+                break
+            cur_level+=1
+            size=len(q)
+            for i in range(0,size):
+                node=q.popleft()
+                if node.left and node.left not in visited:
+                    visited.add(node.left)
+                    q.append(node.left)
+                if node.right and node.right not in visited:
+                    visited.add(node.right)
+                    q.append(node.right)
+                if node in parent and parent[node] and parent[node] not in visited:
+                    visited.add(parent[node])
+                    q.append(parent[node])
+        return [node.val for node in q]
+
+    def parentMap(self,root):
+        d = {root: None} 
+        q=[]
+        q.append(root)
+        while q:
+            node=q.pop()
+            if node.left:
+                d[node.left]=node
+                q.append(node.left)
+            if node.right:
+                d[node.right]=node
+                q.append(node.right)
+        return d
+        
+        
