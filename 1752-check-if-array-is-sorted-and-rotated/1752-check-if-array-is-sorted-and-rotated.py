2620d26d5b258b559babba5a6c9cd479d7d7258d
Srilekha Kasha <147143658+Srilekha-03@users.noreply.github.com>
2025-02-02 11:12:55 +0530

    Time: 0 ms (100%), Space: 12.3 MB (66.26%) - LeetHub

--- 1752-check-if-array-is-sorted-and-rotated/1752-check-if-array-is-sorted-and-rotated.py
+++ 1752-check-if-array-is-sorted-and-rotated/1752-check-if-array-is-sorted-and-rotated.py
@@ -1,22 +1,16 @@
 class Solution(object):
-    def check(self, arr):
-        s=arr[0]
-        d=0
-        for i in range(len(arr)):
-            if arr[i]<s:
-                s=arr[i]
-                d=i
-        for i in range(1,d):
-            if arr[i]<arr[i-1]:
-                return False
-        for i in range(d+1,len(arr)):
-            if arr[i]<arr[i-1]:
-                return False
-        return True  
+    def check(self, nums):
+        count_breaks = 0 
+        n = len(nums)
         
-                
-        """
-        :type nums: List[int]
-        :rtype: bool
-        """
+        for i in range(n):
+            if nums[i] > nums[(i + 1) % n]:
+                count_breaks += 1
+            
+        
+            if count_breaks > 1:
+                return False
+        
+        return True
+
         
