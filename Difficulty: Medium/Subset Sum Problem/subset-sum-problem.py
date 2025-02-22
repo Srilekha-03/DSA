53041e18e66263f4ce05d0c5165ae7c4345749b9
Srilekha Kasha <147143658+Srilekha-03@users.noreply.github.com>
2025-02-23 00:39:48 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Subset Sum Problem/subset-sum-problem.py
+++ Difficulty: Medium/Subset Sum Problem/subset-sum-problem.py
@@ -2,21 +2,33 @@
 
 class Solution:
     def isSubsetSum (self, arr, target):
-        dp=[[-1 for i in range(target+1)]for j in range(len(arr))]
-        return self.recursion(len(arr)-1,arr,target,dp)
-    def recursion(self,index,arr,target,dp):
-        if target==0:
-            return True
-        if index==0:
-            return arr[0]==target
-        if dp[index][target]!=-1:
-            return dp[index][target]
-        not_take=self.recursion(index-1,arr,target,dp)
-        take=False
-        if target>=arr[index]:
-            take=self.recursion(index-1,arr,target-arr[index],dp)
-        dp[index][target]= take or not_take
-        return dp[index][target]
+        dp=[[0 for i in range(target+1)]for j in range(len(arr))]
+        for i in range(len(arr)):
+            dp[i][0]=True
+        if arr[0] <= target:
+            dp[0][arr[0]]=True
+        for index in range(1,len(arr)):
+            for targ in range(target+1):
+                not_take=dp[index-1][targ]
+                take=False
+                if target>=arr[index]:
+                    take=dp[index-1][targ-arr[index]]
+                dp[index][targ]=take or not_take
+        return dp[len(arr)-1][target]
+                
+    # def recursion(self,index,arr,target,dp):
+    #     if target==0:
+    #         return True
+    #     if index==0:
+    #         return arr[0]==target
+    #     if dp[index][target]!=-1:
+    #         return dp[index][target]
+    #     not_take=self.recursion(index-1,arr,target,dp)
+    #     take=False
+    #     if target>=arr[index]:
+    #         take=self.recursion(index-1,arr,target-arr[index],dp)
+    #     dp[index][target]= take or not_take
+    #     return dp[index][target]
             
         # code here 
         
