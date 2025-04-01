72171ac896f6c52116d5a2582e4654e263d642d8
Srilekha Kasha <147143658+Srilekha-03@users.noreply.github.com>
2025-04-01 21:55:38 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Find nth root of m/find-nth-root-of-m.py
+++ Difficulty: Easy/Find nth root of m/find-nth-root-of-m.py
@@ -1,14 +1,25 @@
 #User function Template for python3
 
 class Solution:
-	def nthRoot(self, n: int, m: int) -> int:
-	    low=1
+    def nthRoot(self, n: int, m: int) -> int:
+        def function(mid,n,m):
+            ans=1
+            for i in range(n):
+                ans=ans*mid
+                if ans>m:
+                    return 2
+            if ans==m:
+                return 1
+            return 0
+                
+        low=1
         high=m
         while low<=high:
             mid=(low+high)//2
-            if mid**n==m:
+            midNth=function(mid,n,m)
+            if midNth==1:
                 return mid
-            elif mid**n<m:
+            elif midNth==0:
                 low=mid+1
             else:
                 high=mid-1
@@ -19,6 +30,7 @@ class Solution:
 #{ 
  # Driver Code Starts
 #Initial Template for Python 3
+#Position this line where user code will be pasted.
 
 if __name__ == '__main__':
     T = int(input())
@@ -28,5 +40,6 @@ if __name__ == '__main__':
         ob = Solution()
         ans = ob.nthRoot(n, m)
         print(ans)
+        print("~")
 
 # } Driver Code Ends
