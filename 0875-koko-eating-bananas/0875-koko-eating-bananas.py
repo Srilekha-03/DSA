1db9d7c51beeb7d6f395cde53209173ec6fd9432
Srilekha Kasha <147143658+Srilekha-03@users.noreply.github.com>
2025-04-01 22:36:21 +0530

    Time: 145 ms (86.75%), Space: 18.9 MB (76.25%) - LeetHub

--- 0875-koko-eating-bananas/0875-koko-eating-bananas.py
+++ 0875-koko-eating-bananas/0875-koko-eating-bananas.py
@@ -3,16 +3,14 @@ class Solution(object):
     def minEatingSpeed(self, piles, h):
         low=1
         high=max(piles)
-        ans=high
         while low<=high:
             mid=(low+high)//2
             total_hours=self.total_hours(piles,mid)
             if total_hours<=h:
-                ans=mid
                 high=mid-1
             else:
                 low=mid+1
-        return ans
+        return low
 
     def total_hours(self, piles, hour):
         total = 0
