db04dc46273f878a4824b759926bdd836bec2363
Srilekha Kasha <147143658+Srilekha-03@users.noreply.github.com>
2025-05-18 16:49:43 +0530

    Time: 4 ms (28.6%), Space: 17.8 MB (71.2%) - LeetHub

--- 0316-remove-duplicate-letters/0316-remove-duplicate-letters.py
+++ 0316-remove-duplicate-letters/0316-remove-duplicate-letters.py
@@ -1,21 +1,21 @@
 class Solution:
     def removeDuplicateLetters(self, s: str) -> str:
         n = len(s)
-        result = ""    
+        stack = []    
         taken = [False] * 26
-        last_index = {}       
+        last_index = {}     
         for i, char in enumerate(s):
-            last_index[char] = i
-        
+            last_index[char] = i     
         for i in range(n):
             char = s[i]
-            idx = ord(char) - ord('a')           
+            idx = ord(char) - ord('a')
             if taken[idx]:
-                continue        
-            while result and char < result[-1] and last_index.get(result[-1]) > i:
-                taken[ord(result[-1]) - ord('a')] = False
-                result = result[:-1]          
-            result += char
+                continue
+            while stack and char < stack[-1] and last_index.get(stack[-1]) > i:
+                removed_char = stack.pop()
+                taken[ord(removed_char) - ord('a')] = False
+            
+            stack.append(char)
             taken[idx] = True
         
-        return result
+        return ''.join(stack)
