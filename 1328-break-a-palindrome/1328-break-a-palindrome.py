85f475413f303a52a984ef130d7a4c407ee3331f
Srilekha Kasha <147143658+Srilekha-03@users.noreply.github.com>
2025-05-20 13:02:49 +0530

    Time: 0 ms (100%), Space: 18 MB (9.04%) - LeetHub

---
+++ 1328-break-a-palindrome/1328-break-a-palindrome.py
@@ -0,0 +1,9 @@
+class Solution:
+    def breakPalindrome(self, pal: str) -> str:
+        n = len(pal)
+        if n == 1:
+            return ""     
+        for i in range(n // 2):
+            if pal[i] != 'a':
+                return pal[:i] + 'a' + pal[i+1:]      
+        return pal[:-1] + 'b'
