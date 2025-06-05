aab36475cfd12f7e008f74fcc245ac452ce9c696
Srilekha Kasha <147143658+Srilekha-03@users.noreply.github.com>
2025-06-05 19:58:23 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Subset Sum Problem/subset-sum-problem.py
+++ Difficulty: Medium/Subset Sum Problem/subset-sum-problem.py
@@ -1,59 +1,22 @@
-#User function Template for python3
-
 class Solution:
-    def isSubsetSum (self, arr, target):
-        dp=[[0 for i in range(target+1)]for j in range(len(arr))]
-        for i in range(len(arr)):
-            dp[i][0]=True
-        if arr[0] <= target:
-            dp[0][arr[0]]=True
-        for index in range(1,len(arr)):
-            for targ in range(target+1):
-                not_take=dp[index-1][targ]
-                take=False
-                if target>=arr[index]:
-                    take=dp[index-1][targ-arr[index]]
-                dp[index][targ]=take or not_take
-        return dp[len(arr)-1][target]
-                
-    # def recursion(self,index,arr,target,dp):
-    #     if target==0:
-    #         return True
-    #     if index==0:
-    #         return arr[0]==target
-    #     if dp[index][target]!=-1:
-    #         return dp[index][target]
-    #     not_take=self.recursion(index-1,arr,target,dp)
-    #     take=False
-    #     if target>=arr[index]:
-    #         take=self.recursion(index-1,arr,target-arr[index],dp)
-    #     dp[index][target]= take or not_take
-    #     return dp[index][target]
-            
+    def isSubsetSum(self, arr, sum):
+        return self.helper(0, arr, sum)
+    
+    def helper(self, index, arr, target):
+        # Base cases
+        if target == 0:
+            return True  # Found a valid subset
+        if index == len(arr) or target < 0:
+            return False  # No more elements or impossible target
+        
+        # Include current element OR exclude current element
+        return (self.helper(index + 1, arr, target - arr[index]) or  # Include
+                self.helper(index + 1, arr, target)) 
+        
+        
+    
+        
+    
         # code here 
         
         
-
-
-#{ 
- # Driver Code Starts
-#Initial Template for Python 3
-
-if __name__ == '__main__':
-    t = int(input())
-    for _ in range(t):
-        arr = input().split()
-        N = len(arr)
-        for itr in range(N):
-            arr[itr] = int(arr[itr])
-        sum = int(input())
-
-        ob = Solution()
-        if ob.isSubsetSum(arr, sum) == True:
-            print("true")
-        else:
-            print("false")
-
-        print("~")
-
-# } Driver Code Ends
