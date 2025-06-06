2bca46ea9aa0ccf5b268d51b57e6ab2d2d6f98f5
Srilekha Kasha <147143658+Srilekha-03@users.noreply.github.com>
2025-06-06 23:52:41 +0530

    Time: 26 ms (22.7%), Space: 18.1 MB (53.82%) - LeetHub

--- 0047-permutations-ii/0047-permutations-ii.py
+++ 0047-permutations-ii/0047-permutations-ii.py
@@ -1,8 +1,23 @@
-from itertools import permutations
 class Solution:
     def permuteUnique(self, nums: List[int]) -> List[List[int]]:
-        perms=permutations(nums)
-        unique=set(perms)
-        return [list(i) for i in unique]
-
+        ans=[]
+        ds=[]
+        visited=[0]*len(nums)
+    
+        nums.sort()
+        def permutation(nums,index,ds,visited,ans):
+            if index==len(nums):
+                ans.append(ds[:])
+                return
+            for i in range(len(nums)):
+                if i>0 and nums[i]==nums[i-1] and visited[i-1]==1:
+                    continue
+                if visited[i]!=1:
+                    visited[i]=1
+                    ds.append(nums[i])
+                    permutation(nums,index+1,ds ,visited,ans)
+                    visited[i]=0
+                    ds.pop()
+        permutation(nums,0,ds,visited,ans)
+        return ans
         
