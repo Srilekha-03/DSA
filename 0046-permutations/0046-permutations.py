12c3363f7feddd5251b6488b1d295a92efdbcbaa
Srilekha Kasha <147143658+Srilekha-03@users.noreply.github.com>
2025-06-06 23:32:08 +0530

    Time: 0 ms (100%), Space: 17.8 MB (81.47%) - LeetHub

--- 0046-permutations/0046-permutations.py
+++ 0046-permutations/0046-permutations.py
@@ -1,18 +1,20 @@
-from itertools import permutations
 class Solution:
     def permute(self, nums: List[int]) -> List[List[int]]:
-        def next_permutation(arr):
-            i=len(arr)-2
-            while i>=0 and arr[i]>=arr[i+1]: i-=1
-            if i==-1: return False
-            j=len(arr)-1
-            while arr[j]<=arr[i]: j-=1
-            arr[i],arr[j]=arr[j],arr[i]
-            arr[i+1:]=reversed(arr[i+1:])
-            return True
-
-        nums.sort()
-        res=[nums[:]]
-        while next_permutation(nums): res.append(nums[:])
-        return res
+        ans=[]
+        ds=[]
+        visited=[0]*len(nums)
+        def permutation(nums,index,ds,visited,ans):
+            if index==len(nums):
+                ans.append(ds[:])
+                return
+            for i in range(len(nums)):
+                if visited[i]!=1:
+                    visited[i]=1
+                    ds.append(nums[i])
+                    permutation(nums,index+1,ds ,visited,ans)
+                    visited[i]=0
+                    ds.pop()
+        permutation(nums,0,ds,visited,ans)
+        return ans
+                
         
