ff96b8fabbf006c39b893eeac9554ab34e138178
Srilekha Kasha <147143658+Srilekha-03@users.noreply.github.com>
2025-06-06 23:35:56 +0530

    Time: 0 ms (100%), Space: 17.8 MB (81.47%) - LeetHub

--- 0046-permutations/0046-permutations.py
+++ 0046-permutations/0046-permutations.py
@@ -1,20 +1,15 @@
 class Solution:
     def permute(self, nums: List[int]) -> List[List[int]]:
         ans=[]
-        ds=[]
-        visited=[0]*len(nums)
-        def permutation(nums,index,ds,visited,ans):
+        def permutation(nums,index,ans):
             if index==len(nums):
-                ans.append(ds[:])
+                ans.append(nums[:])
                 return
-            for i in range(len(nums)):
-                if visited[i]!=1:
-                    visited[i]=1
-                    ds.append(nums[i])
-                    permutation(nums,index+1,ds ,visited,ans)
-                    visited[i]=0
-                    ds.pop()
-        permutation(nums,0,ds,visited,ans)
+            for i in range(index,len(nums)):
+                nums[i],nums[index]=nums[index],nums[i]
+                permutation(nums,index+1,ans)
+                nums[i],nums[index]=nums[index],nums[i]                  
+        permutation(nums,0,ans)
         return ans
                 
         
