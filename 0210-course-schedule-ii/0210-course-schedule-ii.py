56c0dbabf0fe04a11351c08252eda1bc7edc57b2
Srilekha Kasha <147143658+Srilekha-03@users.noreply.github.com>
2025-07-17 22:40:50 +0530

    Time: 0 ms (100%), Space: 19.2 MB (60.71%) - LeetHub

---
+++ 0210-course-schedule-ii/0210-course-schedule-ii.py
@@ -0,0 +1,25 @@
+from collections import defaultdict
+class Solution:
+    def findOrder(self, V: int, edges: List[List[int]]) -> List[int]:
+        adj=defaultdict(list)
+        res=[]
+        queue=[]
+        indegree=[0]*V
+        for u,v in edges:
+            adj[u].append(v)
+            indegree[v]+=1
+        for i in range(V):
+            if indegree[i]==0:
+                queue.append(i)
+        while queue:
+            node=queue.pop(0)
+            res.append(node)
+            for j in adj[node]:
+                indegree[j]-=1
+                if indegree[j]==0:
+                    queue.append(j)
+        if len(res)==V:
+            return res[::-1]
+        else:
+            return []
+        
