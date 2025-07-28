c241f7d5598dbeef0fbb9984cc138f44a60e3549
Srilekha Kasha <147143658+Srilekha-03@users.noreply.github.com>
2025-07-28 15:28:45 +0530

    Time: 4 ms (69.65%), Space: 21.3 MB (40.08%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
@@ -2,17 +2,14 @@ from collections import Counter
 class Solution:
     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
         freq=Counter(nums)
-        sorted_dict= dict(sorted(freq.items(),key= lambda x: x[1],reverse=True))
+        sorted_dict= sorted(freq.items(),key= lambda x: x[1],reverse=True)
         res=[]
-        c=0
-        for key,val in sorted_dict.items():
-            if c<k:
-                res.append(key)
-                c+=1
-            else:
-                break
+        i=0
+        while k:
+            res.append(sorted_dict[i][0])
+            i+=1
+            k-=1
         return res
-
 
 
         
