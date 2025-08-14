c800f4d6b9ad4fc4acbbb3892a1abe2bd68652d6
Srilekha Kasha <147143658+Srilekha-03@users.noreply.github.com>
2025-08-14 15:47:56 +0530

    Time: 0 ms (100%), Space: 17.9 MB (10.74%) - LeetHub

--- 0191-number-of-1-bits/0191-number-of-1-bits.py
+++ 0191-number-of-1-bits/0191-number-of-1-bits.py
@@ -2,8 +2,7 @@ class Solution:
     def hammingWeight(self, n: int) -> int:
         count=0
         while n!=0:
-            #to make the rightmost set bit to unset
-            n=n/2
-            count+=(count%2)
-        return count
+            count+=(n%2)
+            n=n//2
+        return int(count)
         
