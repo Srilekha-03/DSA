d49ca3e4d922b0a19019aad3f101217d4f295e55
Srilekha Kasha <147143658+Srilekha-03@users.noreply.github.com>
2025-08-14 14:50:16 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0137-single-number-ii/0137-single-number-ii.py
+++ 0137-single-number-ii/0137-single-number-ii.py
@@ -1,18 +1,19 @@
 class Solution:
     def singleNumber(self, nums: List[int]) -> int:
-        result = 0        
-        for i in range(32):
-            bit = 1 << i
-            countOne = 0            
+        result=0
+        for k in range(32):
+            countZero=0
+            countOne=0
             for num in nums:
-                if num & bit:
-                    countOne += 1          
-            if countOne % 3:
-                result |= bit
-        # Handling negative numbers
-        if result >= 2**31:
-            result -= 2**32
-
+                if (num&(1<<k))==0:
+                    countZero+=1                  
+                else:
+                    countOne+=1
+            if countOne%3!=0:
+                result=result|(1<<k)
         return result
+
+
+
 
         
