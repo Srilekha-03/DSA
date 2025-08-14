9a815e5be03047d5887011d8b45016824b6bb94a
Srilekha Kasha <147143658+Srilekha-03@users.noreply.github.com>
2025-08-14 11:08:55 +0530

    Time: 96 ms (32.32%), Space: 55 MB (74.55%) - LeetHub

--- 3355-zero-array-transformation-i/3355-zero-array-transformation-i.py
+++ 3355-zero-array-transformation-i/3355-zero-array-transformation-i.py
@@ -1,15 +1,15 @@
 class Solution:
     def isZeroArray(self, nums: List[int], queries: List[List[int]]) -> bool:
-        n = len(nums)
-        diff = [0] * (n + 1)  
-        for start, end in queries:
-            diff[start] += 1
-            if end + 1 < n:
-                diff[end + 1] -= 1  
-        current = 0
+        n=len(nums)
+        diff=[0]*n
+        for l,r in queries:
+            diff[l]+=1
+            if r+1<n:
+                diff[r+1]-=1
+        for i in range(1,n):
+            diff[i]=diff[i]+diff[i-1]
         for i in range(n):
-            current += diff[i]
-            if current < nums[i]:
+            if diff[i]<nums[i]:
                 return False
         return True
         
