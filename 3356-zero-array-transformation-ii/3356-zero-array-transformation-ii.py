2c1c8c2f8ed2704940d4f477eccf16350f344e51
Srilekha Kasha <147143658+Srilekha-03@users.noreply.github.com>
2025-08-14 12:04:03 +0530

    Time: 1148 ms (24.43%), Space: 63.9 MB (46.63%) - LeetHub

--- 3356-zero-array-transformation-ii/3356-zero-array-transformation-ii.py
+++ 3356-zero-array-transformation-ii/3356-zero-array-transformation-ii.py
@@ -1,19 +1,32 @@
 class Solution(object):
     def minZeroArray(self, nums, queries):
         n = len(nums)
-        sum_value = 0
-        query_count = 0
-        diff_array = [0] * (n + 1)
-        for i in range(n):
-            while sum_value + diff_array[i] < nums[i]:
-                query_count += 1
-                if query_count > len(queries):
-                    return -1
-                left, right, value = queries[query_count - 1]
-                if right >= i:
-                    diff_array[max(left, i)] += value
-                    if right + 1 < len(diff_array):
-                        diff_array[right + 1] -= value
-            sum_value += diff_array[i]
-        return query_count
+        if all(x == 0 for x in nums):
+            return 0
+
+        def can_zero(k):
+            diff = [0] * (n + 1)
+            for i in range(k):
+                l, r, v = queries[i]
+                diff[l] += v
+                if r + 1 < n:
+                    diff[r + 1] -= v
+
+            curr = 0
+            for i in range(n):
+                curr += diff[i]
+                if nums[i] > curr:
+                    return False
+            return True
+
+        lo, hi = 0, len(queries)
+        ans = -1
+        while lo <= hi:
+            mid = (lo + hi) // 2
+            if can_zero(mid):
+                ans = mid
+                hi = mid - 1
+            else:
+                lo = mid + 1
+        return ans
         
