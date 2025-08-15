ef77c6a8b6157bc2663438212a77376269235b84
Srilekha Kasha <147143658+Srilekha-03@users.noreply.github.com>
2025-08-15 17:01:42 +0530

    Time: 0 ms (100%), Space: 12.8 MB (48.48%) - LeetHub

--- 0785-is-graph-bipartite/0785-is-graph-bipartite.py
+++ 0785-is-graph-bipartite/0785-is-graph-bipartite.py
@@ -10,7 +10,7 @@ class Solution(object):
                     node = queue.popleft()
                     for neighbor in graph[node]:
                         if color[neighbor] == -1:
-                            color[neighbor] = int(not color[node])
+                            color[neighbor] = 1 - color[node]
                             queue.append(neighbor)
                         elif color[neighbor] == color[node]:
                             return False 
