92b82818f8465f8e7e811fb1e5862f53d7d2aadb
Srilekha Kasha <147143658+Srilekha-03@users.noreply.github.com>
2025-08-15 17:10:27 +0530

    Time: 0 ms (100%), Space: 18.7 MB (24.1%) - LeetHub

--- 0785-is-graph-bipartite/0785-is-graph-bipartite.py
+++ 0785-is-graph-bipartite/0785-is-graph-bipartite.py
@@ -1,22 +1,20 @@
 class Solution:
     def isBipartite(self, graph: List[List[int]]) -> bool:
-        n=len(graph)
-        colorr=[-1]*n
+        n = len(graph)
+        colorr = [-1] * n
 
-        def dfs(index,color):
-            colorr[index]=color
+        def dfs(index, color):
+            colorr[index] = color
             for chi in graph[index]:
-                if colorr[chi]==color:
+                if colorr[chi] == color:
                     return False
-                elif colorr[chi]==-1:
-                    if dfs(chi,1-color)==False:
-                        return False
+                elif colorr[chi] == -1 and not dfs(chi, 1 - color):
+                    return False
             return True
 
         for i in range(n):
-            if colorr[i]==-1:
-                if dfs(i,0)==False:
-                    return False
+            if colorr[i] == -1 and not dfs(i, 0):
+                return False
         return True
 
         
