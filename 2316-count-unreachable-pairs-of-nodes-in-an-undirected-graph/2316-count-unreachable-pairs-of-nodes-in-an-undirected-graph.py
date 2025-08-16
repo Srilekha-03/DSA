fd5d7320e2feecd4f72d629d7fd5d21b6401b3f3
Srilekha Kasha <147143658+Srilekha-03@users.noreply.github.com>
2025-08-16 20:23:50 +0530

    Time: 195 ms (68.08%), Space: 66.5 MB (56.81%) - LeetHub

---
+++ 2316-count-unreachable-pairs-of-nodes-in-an-undirected-graph/2316-count-unreachable-pairs-of-nodes-in-an-undirected-graph.py
@@ -0,0 +1,31 @@
+from collections import Counter
+class Solution:
+    def countPairs(self, n: int, edges: List[List[int]]) -> int:
+        parent=[i for i in range(n)]
+        rank=[0]*n
+        count=0
+        def find(x):
+            if parent[x]==x:
+                return x
+            parent[x]=find(parent[x])
+            return parent[x]
+        def union(x,y):
+            px=find(x)
+            py=find(y)
+            if px==py:
+                return 
+            if rank[px]>rank[py]:
+                parent[py]=px
+            elif rank[py]>rank[px]:
+                parent[px]=py
+            else:
+                parent[px]=py
+                rank[py]+=1
+        for u,v in edges:
+            if find(u)!=find(v):
+                union(u,v)
+        comp_sizes=Counter(find(i) for i in range(n))
+        total_pairs = n*(n-1)//2
+        inside_pairs = sum(size*(size-1) // 2 for size in comp_sizes.values())
+
+        return total_pairs - inside_pairs
