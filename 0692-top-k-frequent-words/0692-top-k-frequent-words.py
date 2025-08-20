d743d10fc281c2b1914642495db46f2329235fe5
Srilekha Kasha <147143658+Srilekha-03@users.noreply.github.com>
2025-08-20 20:22:42 +0530

    Time: 1 ms (66.41%), Space: 17.9 MB (39.9%) - LeetHub

--- 0692-top-k-frequent-words/0692-top-k-frequent-words.py
+++ 0692-top-k-frequent-words/0692-top-k-frequent-words.py
@@ -2,7 +2,11 @@ from collections import Counter
 class Solution:
     def topKFrequent(self, words: List[str], k: int) -> List[str]:
         freq=Counter(words)
-        sorted_words = sorted(freq.keys(), key=lambda w: (-freq[w], w))
+        # First sort alphabetically
+        sorted_words = sorted(freq.keys())
+
+        # Then sort by frequency (descending), stable keeps alphabetical order for ties
+        sorted_words = sorted(sorted_words, key=lambda w: freq[w], reverse=True)
         return sorted_words[:k]
 
 
