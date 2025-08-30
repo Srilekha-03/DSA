4ac164ff7186470c0109f1e62cee7c7833ddbfec
Srilekha Kasha <147143658+Srilekha-03@users.noreply.github.com>
2025-08-30 12:11:02 +0530

    Time: 7 ms (37.12%), Space: 17.8 MB (69.74%) - LeetHub

--- 0036-valid-sudoku/0036-valid-sudoku.py
+++ 0036-valid-sudoku/0036-valid-sudoku.py
@@ -1,17 +1,44 @@
 class Solution:
- def isValidSudoku(self,board:List[List[str]])->bool:
-    rows=[set()for i in range(9)]
-    cols=[set()for i in range(9)]
-    boxes=[set()for i in range(9)]
-    for i in range(9):
-        for j in range(9):
-            num=board[i][j]
-            if num=='.':
-                continue
-            box=(i//3)*3+(j//3)
-            if num in rows[i]or num in cols[j]or num in boxes[box]:
-                return False
-            rows[i].add(num);cols[j].add(num);boxes[box].add(num)
-    return True
+    def validSub(self, board: List[List[str]], sr: int, er: int, sc: int, ec: int) -> bool:
+        st = set()
+        for row in range(sr, er + 1):
+            for col in range(sc, ec + 1):
+                ch = board[row][col]
+                if ch == '.':
+                    continue
+                if ch in st:
+                    return False
+                st.add(ch)
+        return True
+
+    def isValidSudoku(self, board: List[List[str]]) -> bool:
+        for row in range(9):
+            st = set()
+            for col in range(9):
+                ch = board[row][col]
+                if ch == '.':
+                    continue
+                if ch in st:
+                    return False
+                st.add(ch)
+
+        for col in range(9):
+            st = set()
+            for row in range(9):
+                ch = board[row][col]
+                if ch == '.':
+                    continue
+                if ch in st:
+                    return False
+                st.add(ch)
+
+        for sr in range(0, 9, 3):
+            er = sr + 2
+            for sc in range(0, 9, 3):
+                ec = sc + 2
+                if not self.validSub(board, sr, er, sc, ec):
+                    return False
+
+        return True
 
         
