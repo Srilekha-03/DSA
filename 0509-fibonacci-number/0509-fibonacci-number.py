c058771b863b1ec33c40e9751646ea3023b6866f
Srilekha Kasha <147143658+Srilekha-03@users.noreply.github.com>
2025-09-05 23:46:55 +0530

    Time: 48 ms (37.42%), Space: 17.9 MB (23.32%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.py
+++ 0509-fibonacci-number/0509-fibonacci-number.py
@@ -1,11 +1,14 @@
 class Solution:
     def fib(self, n: int) -> int:
-        dp=[0]*(n+1)
-        dp[0]=0
-        if n>0:
-            dp[1]=1
-        for i in range(2,n+1):
-            dp[i]=dp[i-1]+dp[i-2]
-        return dp[n]
+        if n<=1:
+            return n
+        a=0
+        b=1
+        c=0
+        for i in range(n-1):
+            c=a+b
+            a=b
+            b=c
+        return c
 
         
