ecbda3024fc6cfc0d04980b6f271aa31de80e287
Srilekha Kasha <147143658+Srilekha-03@users.noreply.github.com>
2025-09-05 11:59:36 +0530

    Time: 88 ms (58.14%), Space: 28.7 MB (69.99%) - LeetHub

--- 0215-kth-largest-element-in-an-array/0215-kth-largest-element-in-an-array.py
+++ 0215-kth-largest-element-in-an-array/0215-kth-largest-element-in-an-array.py
@@ -5,7 +5,8 @@ class Solution:
         heapq.heapify(min_heap)
 
         for num in nums[k:]:
-            if num>min_heap[0]:
-                heapq.heappushpop(min_heap,num)  # Replace the smallest
+            heapq.heappush(min_heap,num)
+            if len(min_heap)>k:
+                 heapq.heappop(min_heap)
         return min_heap[0]
         
