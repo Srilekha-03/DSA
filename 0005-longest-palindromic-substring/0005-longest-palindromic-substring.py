d61aaceb2046689afbfc9efb76f5ff6fd3e6bcf5
Srilekha Kasha <147143658+Srilekha-03@users.noreply.github.com>
2025-09-13 10:02:47 +0530

    Time: 3828 ms (16.88%), Space: 26.2 MB (5.85%) - LeetHub

--- 0005-longest-palindromic-substring/0005-longest-palindromic-substring.py
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.py
@@ -1,29 +1,28 @@
 class Solution:
-    def __init__(self):
-        self.dp = None
-
-    def solve(self, s, l, r):
-        if l >= r:
-            return True
-        if self.dp[l][r] != -1:
-            return self.dp[l][r]
-        if s[l] == s[r]:
-            self.dp[l][r] = self.solve(s, l + 1, r - 1)
-        else:
-            self.dp[l][r] = False
-        return self.dp[l][r]
-
     def longestPalindrome(self, s: str) -> str:
         n = len(s)
-        self.dp = [[-1 for _ in range(n)] for _ in range(n)]
-        maxlen = 0
-        startIndex = 0  
-        for i in range(n):
-            for j in range(i, n):
-                if self.solve(s, i, j):
-                    if j - i + 1 > maxlen:
-                        maxlen = j - i + 1
-                        startIndex = i
-        return s[startIndex:startIndex + maxlen]
-                    
+        count = 0
+        dp=[[False for i in range(n)]for j in range(n)]
+        count=0
+        maxlen=0
+        st=0
+        end=0
+        for L in range(1,n+1):
+            for i in range(0,n-L+1):
+                j=i+L-1
+                if i==j:
+                    dp[i][j]=True
+                elif s[i]==s[j]:
+                    if i+1==j:
+                        dp[i][j]=True
+                    else:
+                        dp[i][j]=dp[i+1][j-1]
+                if dp[i][j]==True:
+                    maxlen=max(maxlen,j-i+1)
+                    st=i
+                    end=j
+        if maxlen==0:
+            return ""
+        else:
+            return s[st:end+1]
         
