69b00d4b5eae5a389ff7247e1a948bcd2a22813a
Srilekha Kasha <147143658+Srilekha-03@users.noreply.github.com>
2025-09-28 15:26:55 +0530

    Time: 1 ms (60.18%), Space: 18 MB (31.14%) - LeetHub

--- 0889-construct-binary-tree-from-preorder-and-postorder-traversal/0889-construct-binary-tree-from-preorder-and-postorder-traversal.py
+++ 0889-construct-binary-tree-from-preorder-and-postorder-traversal/0889-construct-binary-tree-from-preorder-and-postorder-traversal.py
@@ -6,14 +6,24 @@
 #         self.right = right
 class Solution:
     def constructFromPrePost(self, preorder: List[int], postorder: List[int]) -> Optional[TreeNode]:
-        if not preorder:
-            return None
-        root = TreeNode(preorder[0])
+        n=len(preorder)
+        self.ind=0
+        post_index={val:i for i,val in enumerate(postorder)}
+        def helper(start,end):
+            if start>end :
+                return None
 
-        if len(preorder) == 1:
+            rootval=preorder[self.ind]
+            i=post_index[rootval]
+            root=TreeNode(rootval)
+            self.ind+=1
+            if start == end or self.ind >= n:
+                return root
+            leftval=preorder[self.ind]
+            leftind=post_index[leftval]
+    
+            root.left=helper(start,leftind)
+            root.right=helper(leftind+1,end-1)
             return root
-
-        left_subtree_root_index = postorder.index(preorder[1])
-        root.left = self.constructFromPrePost(preorder[1:left_subtree_root_index+2], postorder[:left_subtree_root_index+1])
-        root.right = self.constructFromPrePost(preorder[left_subtree_root_index+2:], postorder[left_subtree_root_index+1:-1])
-        return root
+        return helper(0,n-1)
+        
