7388f86193613d06582c68071a15d390cf8129ab
Srilekha Kasha <147143658+Srilekha-03@users.noreply.github.com>
2025-10-06 21:23:40 +0530

    Time: 4 ms (34.75%), Space: 23.3 MB (38.02%) - LeetHub

--- 0143-reorder-list/0143-reorder-list.py
+++ 0143-reorder-list/0143-reorder-list.py
@@ -5,26 +5,38 @@
 #         self.next = next
 class Solution:
     def reorderList(self, head: Optional[ListNode]) -> None:
+        if not head or not head.next:
+            return head
+        slow=head
+        fast=head.next
+        while fast and fast.next:
+            fast=fast.next.next
+            slow=slow.next
+        new_head=self.reverse(slow.next)
+        slow.next=None
+        i=head
+        j=new_head
+        while j:
+            temp1=i.next
+            temp2=j.next
+            i.next=j
+            j.next=temp1
+            i=temp1
+            j=temp2
+        return head
+    def reverse(self,root):
+        prev=None
+        cur=root
+        while cur:
+            nex=cur.next
+            cur.next=prev
+            prev=cur
+            cur=nex
+        return prev
+
+
+
         """
         Do not return anything, modify head in-place instead.
         """
-        if not head:
-            return
-
-        st = []
-        curr = head
-        while curr:
-            st.append(curr)
-            curr = curr.next
-
-        k = len(st) // 2
-        curr = head
-        while k > 0:
-            topNode = st.pop()
-            temp = curr.next
-            curr.next = topNode
-            topNode.next = temp
-            curr = temp
-            k -= 1
-        curr.next = None
         
