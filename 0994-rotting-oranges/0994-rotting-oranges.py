174b148bad3a7410d7da991a7faf07ea377da175
Srilekha Kasha <147143658+Srilekha-03@users.noreply.github.com>
2025-10-18 12:49:17 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0994-rotting-oranges/0994-rotting-oranges.py
+++ 0994-rotting-oranges/0994-rotting-oranges.py
@@ -9,28 +9,30 @@ class Solution(object):
         for i in range(n):
             for j in range(m):
                 if grid[i][j] == 2:
-                    q.append((i, j, 0)) 
+                    q.append((i, j)) 
                     vis[i][j] = 2
                 elif grid[i][j] == 1:
                     cnt_fresh += 1
         drow = [-1, 0, 1, 0]
-        dcol = [0, 1, 0, -1]       
-        tm = 0 
+        dcol = [0, 1, 0, -1]  
+        level=0     
         cnt = 0    
         while q:
-            r, c, t = q.popleft()
-            tm = max(tm, t)
-            for i in range(4):
-                nrow = r + drow[i]
-                ncol = c + dcol[i]
-                if 0 <= nrow < n and 0 <= ncol < m:
-                    if vis[nrow][ncol] == 0 and grid[nrow][ncol] == 1:
-                        q.append((nrow, ncol, t + 1))
-                        vis[nrow][ncol] = 2
-                        cnt += 1       
+            size=len(q)
+            for g in range(size):
+                r, c= q.popleft()
+                for i in range(4):
+                    nrow = r + drow[i]
+                    ncol = c + dcol[i]
+                    if 0 <= nrow < n and 0 <= ncol < m:
+                        if vis[nrow][ncol] == 0 and grid[nrow][ncol] == 1:
+                            q.append((nrow, ncol))
+                            vis[nrow][ncol] = 2
+                            cnt += 1
+            level+=1       
         if cnt != cnt_fresh:
             return -1        
-        return tm
+        return level-1
         """
         :type grid: List[List[int]]
         :rtype: int
