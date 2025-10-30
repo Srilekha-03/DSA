c2d52e269cddcc7d4fd53798a531ea9b191c93aa
Srilekha Kasha <147143658+Srilekha-03@users.noreply.github.com>
2025-10-30 21:56:16 +0530

    Time: 371 ms (20.37%), Space: 0B (100%) - LeetHub

--- 0626-exchange-seats/0626-exchange-seats.sql
+++ 0626-exchange-seats/0626-exchange-seats.sql
@@ -3,7 +3,7 @@ SELECT
     CASE
         WHEN id % 2 != 0 AND id = (SELECT COUNT(*) FROM Seat) THEN id
         WHEN id % 2 = 0 THEN id - 1
-        ELSE id + 1
+        WHEN id % 2 != 0 THEN id + 1
     END AS id,
     student
 FROM
