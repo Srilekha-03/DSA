5f51b314ff36b0b7b5393f8aa7ac08b1a34da640
Srilekha Kasha <147143658+Srilekha-03@users.noreply.github.com>
2025-10-31 11:42:57 +0530

    Time: 195 ms (46.35%), Space: 48.7 MB (27.16%) - LeetHub

--- 2359-find-closest-node-to-given-two-nodes/2359-find-closest-node-to-given-two-nodes.py
+++ 2359-find-closest-node-to-given-two-nodes/2359-find-closest-node-to-given-two-nodes.py
@@ -1,20 +1,33 @@
-class Solution(object):
-    def dfs(self, current, distance, edges, distances):
-        while current != -1 and distances[current] == -1:
-            distances[current] = distance
-            distance += 1
-            current = edges[current]
-
-    def closestMeetingNode(self, edges, start1, start2):
-        res, Min_Of_Max, n = -1, float('inf'), len(edges)
-        dist1 = [-1] * n
-        dist2 = [-1] * n
-        self.dfs(start1, 0, edges, dist1)
-        self.dfs(start2, 0, edges, dist2)
+class Solution:
+    def closestMeetingNode(self, edges: List[int], node1: int, node2: int) -> int:
+        if node1==node2:
+            return node1
+        if edges[node1]==-1 and edges[node2]==-1:
+            return -1
+        n=len(edges)
+        d1=[float('inf')]*n
+        d2=[float('inf')]*n
+        d1[node1]=0
+        d2[node2]=0
+        v1=[False]*n
+        v2=[False]*n
+        res=[0]*n
+        self.dfs(node1,edges,d1,v1)
+        self.dfs(node2,edges,d2,v2)
         for i in range(n):
-            if dist1[i] >= 0 and dist2[i] >= 0:
-                maxDist = max(dist1[i], dist2[i])
-                if maxDist < Min_Of_Max:
-                    Min_Of_Max = maxDist
-                    res = i
-        return res
+            if d1[i]>d2[i]:
+                res[i]=d1[i]
+            else:
+                res[i]=d2[i]
+        if min(res)==float('inf'):
+            return -1
+        return res.index(min(res))
+    def dfs(self,node,edges,d,v):
+        v[node]=True
+        if edges[node]!=-1:
+            if v[edges[node]]==False:
+                d[edges[node]]=1+d[node]
+                self.dfs(edges[node],edges,d,v)
+
+
+        
