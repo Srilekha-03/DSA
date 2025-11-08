4bab7578d50c91d729f6ed289522db79c3164e45
Srilekha Kasha <147143658+Srilekha-03@users.noreply.github.com>
2025-11-08 13:05:43 +0530

    Time: 10 ms (34.17%), Space: 20.6 MB (76.75%) - LeetHub

--- 0229-majority-element-ii/0229-majority-element-ii.py
+++ 0229-majority-element-ii/0229-majority-element-ii.py
@@ -1,41 +1,35 @@
-class Solution(object):
-    def majorityElement(self, nums):
-        n=len(nums)
+class Solution:
+    def majorityElement(self, nums: List[int]) -> List[int]:
         c1=0
         c2=0
-        ele1=0
-        ele2=0
-        l=[]
-        for i in nums:
-            if c1==0 and i!=ele2:
-                c1=1
-                ele1=i
-            elif c2==0 and ele1!=i:
-                c2=1
-                ele2=i
-            elif i==ele1:
+        n=len(nums)
+        ele1,ele2=None,None
+        for num in nums:
+            if c1==0 and num!=ele2:
+                ele1=num
+            if c2==0 and num!=ele1:
+                ele2=num
+            if num==ele1:
                 c1+=1
-            elif i==ele2:
+            elif num==ele2:
                 c2+=1
             else:
                 c1-=1
                 c2-=1
-        c1=0 
+        res=[]
+        c1=0
         c2=0
-        for i in nums:
-            if i==ele1:
+        for num in nums:
+            if num==ele1:
                 c1+=1
-            elif i==ele2:
+            elif num==ele2:
                 c2+=1
-        if c1>n/3:
-            l.append(ele1)
-        if c2>n/3:
-            l.append(ele2)
-        return l
-            
+        if c1>n//3:
+            res.append(ele1)
+        if c2>n//3:
+            res.append(ele2)
+        return res
 
-        """
-        :type nums: List[int]
-        :rtype: List[int]
-        """
+        
+
         
