b4f23e8756e23fbbc98c5d905302adf94fb5c9a3
Srilekha Kasha <147143658+Srilekha-03@users.noreply.github.com>
2025-11-11 19:53:59 +0530

    Time: 27 ms (13.17%), Space: 20.1 MB (17.14%) - LeetHub

--- 0135-candy/0135-candy.py
+++ 0135-candy/0135-candy.py
@@ -5,10 +5,10 @@ class Solution:
         right=[1]*n
         for i in range(1,n):
             if ratings[i]>ratings[i-1]:
-                left[i]=max(left[i],left[i-1]+1)
+                left[i]=left[i-1]+1
         for i in range(n-2,-1,-1):
             if ratings[i]>ratings[i+1]:
-                right[i]=max(right[i],right[i+1]+1)
+                right[i]=right[i+1]+1
         res=0
         for i in range(n):
             res+=max(left[i],right[i])
