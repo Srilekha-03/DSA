26afe0235f377243cf38e938fdd3a28b5ae6703a
Srilekha Kasha <147143658+Srilekha-03@users.noreply.github.com>
2025-11-16 10:07:14 +0530

    Time: 0 ms (100%), Space: 17.6 MB (98.89%) - LeetHub

--- 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.py
+++ 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.py
@@ -1,32 +1,22 @@
-class Solution(object):
-    def letterCombinations(self, digits):
-        def backtrack(index, path):
-            if index == len(digits):
-                result.append("".join(path))
-                return
-
-            letters = digit_map[digits[index]]
-
-            for letter in letters:
-                path.append(letter)
-                backtrack(index + 1, path)
-                path.pop()
-
-        result = []
-        digit_map = {
-            '2': 'abc',
-            '3': 'def',
-            '4': 'ghi',
-            '5': 'jkl',
-            '6': 'mno',
-            '7': 'pqrs',
-            '8': 'tuv',
-            '9': 'wxyz'
+class Solution:
+    def letterCombinations(self, digits: str) -> List[str]:
+        self.res=[]
+        self.keypad = {
+            "2": "abc",
+            "3": "def",
+            "4": "ghi",
+            "5": "jkl",
+            "6": "mno",
+            "7": "pqrs",
+            "8": "tuv",
+            "9": "wxyz"
         }
-
-        if digits:
-            backtrack(0, [])
-
-        return result
-   
+        def solve(i,res):
+            if len(res)==len(digits):
+                self.res.append(res)
+                return 
+            for x in self.keypad[digits[i]]:
+                solve(i+1,res+x)
+        solve(0,"")
+        return self.res
         
