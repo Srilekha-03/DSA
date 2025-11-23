9486d414472cea426f6af563649f914597af9e85
Srilekha Kasha <147143658+Srilekha-03@users.noreply.github.com>
2025-11-23 17:56:25 +0530

    Time: 4 ms (56.31%), Space: 17.7 MB (68.79%) - LeetHub

--- 0036-valid-sudoku/0036-valid-sudoku.py
+++ 0036-valid-sudoku/0036-valid-sudoku.py
@@ -1,17 +1,18 @@
 class Solution:
-    def validSub(self, board: List[List[str]], sr: int, er: int, sc: int, ec: int) -> bool:
-        st = set()
-        for row in range(sr, er + 1):
-            for col in range(sc, ec + 1):
-                ch = board[row][col]
-                if ch == '.':
-                    continue
-                if ch in st:
-                    return False
-                st.add(ch)
-        return True
-
     def isValidSudoku(self, board: List[List[str]]) -> bool:
+        def validSub(sr, er, sc, ec):
+            st = set()
+            for row in range(sr, er):
+                for col in range(sc, ec):
+                    ch = board[row][col]
+                    if ch == '.':
+                        continue
+                    if ch in st:
+                        return False
+                    st.add(ch)
+            return True
+
+        # check rows
         for row in range(9):
             st = set()
             for col in range(9):
@@ -22,6 +23,7 @@ class Solution:
                     return False
                 st.add(ch)
 
+        # check columns
         for col in range(9):
             st = set()
             for row in range(9):
@@ -32,13 +34,13 @@ class Solution:
                     return False
                 st.add(ch)
 
+        # check 3×3 boxes
         for sr in range(0, 9, 3):
-            er = sr + 2
+            er = sr + 3
             for sc in range(0, 9, 3):
-                ec = sc + 2
-                if not self.validSub(board, sr, er, sc, ec):
+                ec = sc + 3
+                if not validSub(sr, er, sc, ec):
                     return False
 
         return True
-
         
