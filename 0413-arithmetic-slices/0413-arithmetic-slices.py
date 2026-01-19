4cf0e5f6cfeb34e26d36a65cba1eeccedbdea282
Srilekha Kasha <147143658+Srilekha-03@users.noreply.github.com>
2026-01-19 16:17:46 +0530

    Time: 0 ms (100%), Space: 18 MB (59.83%) - LeetHub

--- 0413-arithmetic-slices/0413-arithmetic-slices.py
+++ 0413-arithmetic-slices/0413-arithmetic-slices.py
@@ -1,16 +1,13 @@
 class Solution:
     def numberOfArithmeticSlices(self, nums: List[int]) -> int:
-        def helper(arr):
-            diff=arr[1]-arr[0]
-            for i in range(2,len(arr)):
-                if arr[i]-arr[i-1]!=diff:
-                    return False
-            return True
-        count=0
-        for i in range(len(nums)):
-            for j in range(i+2,len(nums)):
-                if helper(nums[i:j+1]):
-                    count+=1
+        count = 0
+        curr = 0
+        for i in range(2, len(nums)):
+            if nums[i] - nums[i - 1] == nums[i - 1] - nums[i - 2]:
+                curr += 1
+                count += curr
+            else:
+                curr = 0
         return count
         
         
