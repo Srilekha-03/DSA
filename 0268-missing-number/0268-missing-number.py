9096fe6af05d17824962b915d5d119a10b282614
Srilekha Kasha <147143658+Srilekha-03@users.noreply.github.com>
2026-02-05 12:12:15 +0530

    Time: 1397 ms (5.34%), Space: 20.6 MB (23.05%) - LeetHub

--- 0268-missing-number/0268-missing-number.py
+++ 0268-missing-number/0268-missing-number.py
@@ -1,15 +1,8 @@
-class Solution(object):
-    def missingNumber(self, arr):
-        n=len(arr)
-        xor1=0
-        xor2=0
-        for i in range(n):
-            xor1=xor1^arr[i]
-            xor2=xor2^i
-        xor2=xor2^n
-        return xor1^xor2
-        """
-        :type nums: List[int]
-        :rtype: int
-        """
+class Solution:
+    def missingNumber(self, nums: List[int]) -> int:
+        for i in range(len(nums)+1):
+            if i not in nums:
+                return i
+
+
         
